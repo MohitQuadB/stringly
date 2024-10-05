79a5a9f2edbc0d24e5f517db3075f0667538e0e1
Karansayambar <karansayambar@gmail.com>
2024-10-05 17:52:16 +0530

    navbar and footer added

--- src/stringly2_frontend/src/App.jsx
+++ src/stringly2_frontend/src/App.jsx
@@ -1,30 +1,20 @@
 import { useState } from 'react';
-import { stringly2_backend } from 'declarations/stringly2_backend';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+// import { stringly2_backend } from 'declarations/stringly2_backend';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    stringly2_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
 
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
+    <div>
+      <Navbar/>
+      <Routes>
+        {/* <Route path='/' element={<landingPage/>}/> */}
+      </Routes>
+      <Footer/>
+    </div>
   );
 }
 
---
+++ src/stringly2_frontend/src/components/Navbar.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react'
+import { IoIosArrowDown } from "react-icons/io";
+import { IoIosMenu } from "react-icons/io";
+import { RxCross1 } from "react-icons/rx";
+import { Link } from 'react-router-dom';
+
+
+const Navbar = () => {
+    const [isMenu, setIsmMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setIsmMenu(!isMenu)
+    }
+
+    return (
+        <div className='w-full bg-red bg-gray-200 position-fixed top-0 flex flex-col'>
+            <div className='flex items-center justify-between px-8 py-4'>
+                <div>
+                    <p className='text-2xl font-semibold text-[#DC73B6]'>Logo</p>
+                </div>
+                <div className='hidden md:flex items-center justify-between gap-8'>
+                    <p>
+                        <Link to="/">Home</Link>
+                    </p>
+                    <p>
+                        <Link to="/shop">Features</Link>
+                    </p>
+                    <p>
+                        <Link to="/product">About Us</Link>
+                    </p>
+                    <div className='flex items-center gap-2'>
+                        <Link to="/contact">Resources</Link>
+                        <IoIosArrowDown />
+                    </div>
+                </div>
+                <div className='flex items-center gap-4'>
+                    <div>
+                        <button className={`px-4 py-2 rounded-xl ${isMenu ? 'bg-[#0B0A0A] text-white' : 'bg-[#FAFAFA] text-black'}`}>Join</button>
+                    </div>
+                    <div className='flex md:hidden'>
+                        <button className='text-xl' onClick={() => toggleMenu()}>
+                            {
+                                isMenu ? <RxCross1 /> : <IoIosMenu />
+                            }
+                        </button>
+                    </div>
+                </div>
+            </div>
+            {
+                isMenu && <Menu />
+            }
+        </div>
+    )
+}
+
+export default Navbar
+
+export const Menu = () => {
+    return (
+        <>
+            <span className="bg-white w-full p-4 flex flex-col items-start justify-between gap-6 text-xl">
+                <p>
+                    <Link to="/">Home</Link>
+                </p>
+                <p>
+                    <Link to="/shop">Features</Link>
+                </p>
+                <p>
+                    <Link to="/product">About Us</Link>
+                </p>
+                <div className='flex items-center gap-2'>
+                    <Link to="/contact">Resources</Link>
+                    <IoIosArrowDown />
+                </div>
+            </span>
+        </>
+    );
+};
